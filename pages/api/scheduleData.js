452df9c688c432436a1988b90046afc41435912b
Suhani Singh Paliwal <suhanispaliwal@gmail.com>
2024-09-09 01:19:31 +0530

    Update scheduleData.js

--- pages/api/scheduleData.js
+++ pages/api/scheduleData.js
@@ -1,32 +1,32 @@
 export const schedData = [
     {
         id: 1,
-        duration: '25th Feb to 26th Feb',
+        duration: '29th Sept to 30th Sept',
         event: 'Project Assignment to Mentors'
     },
     {
         id: 2,
-        duration: '01st Mar',
+        duration: '01st Oct',
         event: 'Coding Period Starts'
     },
     {
         id: 3,
-        duration: '5th Mar to 6th Mar',
+        duration: '3rd Oct',
         event: 'Community Bonding Session begins'
     },
     {
         id: 4,
-        duration: '1st April',
+        duration: '5th Oct',
         event: 'Leaderboard Opens'
     },
     {
         id: 5,
-        duration: '31st May',
+        duration: '30th Oct',
         event: 'Coding Period Ends'
     },
     {
         id: 6,
-        duration: '2nd week of July',
+        duration: '2nd week of November',
         event: 'Results will be declared'
     },
 ];
